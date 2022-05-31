006ecc147e414dcfb6a5b6a15c75df8008fffadc
Code Sandbox <hello@codesandbox.io>
2022-05-31 00:53:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.bd6d520b3495d2ea9d1e8ace27f83b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0e5994d3e5ee854731ec72b10da0b8",
+    "url": "https://kitamuradev.github.io/csb-74hv6s/index.html"
+  },
+  {
+    "revision": "785cd783fca0f30c5971",
+    "url": "https://kitamuradev.github.io/csb-74hv6s/static/js/2.25c3a0aa.chunk.js"
+  },
+  {
+    "revision": "e8658e50dfaa215d03e17c686961427f",
+    "url": "https://kitamuradev.github.io/csb-74hv6s/static/js/2.25c3a0aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3765df49db1e013545",
+    "url": "https://kitamuradev.github.io/csb-74hv6s/static/js/main.ebe6bf04.chunk.js"
+  },
+  {
+    "revision": "b9c9b90fdfae89ac2d4a",
+    "url": "https://kitamuradev.github.io/csb-74hv6s/static/js/runtime-main.93d920ba.js"
+  }
+]);
